5df49e78430ec7d1e4d28e1a64d6535b1f74105f
Renan Tashiro <renantashiro@hotmail.com>
2023-06-27 16:34:25 -0300

    refactor(): remove deploy vars

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,15 @@
 #!/bin/bash
 
-# Build da aplicação
+source .env
+
 npm run build
 
-# Nome do bucket S3
-BUCKET_NAME="mamao-landpage-tashiro"
+REGION=$AWS_REGION
+BUCKET_NAME=$S3_BUCKET_NAME
+CLOUDFRONT_DISTRIBUTION=$CLOUDFRONT_DISTRIBUTION_ID
 
-# Região do bucket S3
-REGION="sa-east-1"
-
-# Nome do diretório local com os arquivos do build
-BUILD_DIR="build"
-
-# Nome da distribuição CloudFront
-CLOUDFRONT_DISTRIBUTION="EE7HOT45Y65FF"
-
-# Sincroniza os arquivos do build com o bucket S3
-aws s3 sync $BUILD_DIR s3://$BUCKET_NAME --delete --region $REGION
-
-# Atualiza a distribuição CloudFront
+aws s3 sync build/ s3://$BUCKET_NAME --delete --region $REGION
 aws cloudfront update-distribution --id $CLOUDFRONT_DISTRIBUTION
-
-# Invalida o cache da distribuição CloudFront
 aws cloudfront create-invalidation --distribution-id $CLOUDFRONT_DISTRIBUTION --paths "/*"
+
+echo "Deploy concluído com sucesso!"
